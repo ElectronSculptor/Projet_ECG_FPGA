48428e9a9489d1365d72f34b10ebb6309dec6130
ElectronSculptor <ishkhanyangevor1@gmail.com>
2025-03-05 14:37:47 +0100

    add Check_if

--- FPGA_UART.py
+++ FPGA_UART.py
@@ -3,9 +3,8 @@
 # We gonna try to automatize that and add a class
 
 
+
 import serial
-
-
 
 
 class FPGA_UART:
@@ -36,204 +35,28 @@ class FPGA_UART:
             print("UART connection closed.")
 
     def set_memory_addr(self, address):
-        """Send a command to set the memory address."""
+        """Send a command to set the memory address in bytestring format."""
         if isinstance(address, int):
-            self.serial_conn.write(f"SET_ADDR {address}\n".encode())
+            self.serial_conn.write(bytes([address]))
         else:
             print("Invalid address: Must be an integer.")
 
     def write_val_mem(self, value):
-        """Write a value to the memory."""
+        """Write a value to the memory in bytestring format."""
         if isinstance(value, int):
-            self.serial_conn.write(f"WRITE_MEM {value}\n".encode())
+            self.serial_conn.write(bytes([value]))
         else:
             print("Invalid value: Must be an integer.")
 
     def display_mem_vals_leds(self, value):
-        """Send a command to display memory values on LEDs."""
+        """Send a command to display memory values on LEDs in bytestring format."""
         if isinstance(value, int):
-            self.serial_conn.write(f"DISPLAY_LED {value}\n".encode())
+            self.serial_conn.write(bytes([value]))
         else:
             print("Invalid value: Must be an integer.")
 
     def read_mem_val(self):
-        """Read a value from memory."""
-        self.serial_conn.write(b"READ_MEM\n")
-        response = self.serial_conn.readline().decode().strip()
+        """Read a value from memory and return as a bytestring."""
+        self.serial_conn.write(b"\x01")  # Example read command as a single byte
+        response = self.serial_conn.read(1)  # Read single byte
         return response
-
-
-
-
-
-
-"""
-This module is responsible for implementing the class used to check object types.
-It's implemented by the professor, Raphael VIERA.
-"""
-
-class CheckIf:
-    """
-    This class implements the methods responsible for checking object types
-    """
-
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def is_list(element):
-        """
-        Check if element is of type list.
-
-        Args:
-            - element: list
-
-        Returns:
-            - : bool
-        """
-
-        return isinstance(element, list)
-
-    @staticmethod
-    def is_set(element):
-        """
-        Check if element is of type set.
-
-        Args:
-            - element: set
-
-        Returns:
-            - : bool
-        """
-
-        return isinstance(element, set)
-
-    @staticmethod
-    def is_tuple(element):
-        """
-        Check if element is of type tuple.
-
-        Args:
-            - element: tuple
-
-        Returns:
-            - : bool
-        """
-
-        return isinstance(element, tuple)
-
-    @staticmethod
-    def is_string(element):
-        """
-        Check if element is of type string.
-
-        Args:
-            - element: str
-
-        Returns:
-            - : bool
-        """
-
-        return isinstance(element, str)
-
-    @staticmethod
-    def is_bool(element):
-        """
-        Check if element is of type bool.
-
-        Args:
-            - element: bool
-
-        Returns:
-            - : bool
-        """
-
-        try:
-            if isinstance(element, bool):
-                return True
-            else:
-                return False
-        except:
-            return False
-
-    @staticmethod
-    def is_in_range(val, range):
-        """
-        Check if val is in range.
-
-        Args:
-            - val: float
-            - range: list
-
-        Returns:
-            - : bool
-        """
-
-        try:
-            if min(range) <= val <= max(range):
-                return True
-            else:
-                return False
-        except:
-            return False
-
-    @staticmethod
-    def is_hashable_type(obj):
-        """
-        Check if element is hashable.
-
-        Args:
-            - element: obj
-
-        Returns:
-            - : bool
-        """
-
-        import collections
-        return isinstance(obj, collections.abc.Hashable)
-
-    @staticmethod
-    def is_value_in_enum(user_param_enum, user_param_value):
-        try:
-            get_verification_parameter = getattr(user_param_enum, user_param_value)
-            temp = get_verification_parameter.value
-
-            return True
-        except:
-            # exiting here because the parameter does not exist in the ENUM
-            return False
-
-    @staticmethod
-    def is_number(to_check):
-        """
-        Check if element is of type number even if it's a string that can be converted to a number.
-
-        Args:
-            - to_check: str or int or float or bool
-
-        Returns:
-            - : bool
-        """
-
-        # int(False) = 0 but it is not a number.
-        # 0 == False so checking if to_check is
-        try:
-            if isinstance(to_check, bool):
-                return False
-        except:
-            pass
-
-        try:
-            float(to_check)
-            return True
-        except:  # ValueError:
-            pass
-
-        try:
-            import unicodedata
-            unicodedata.numeric(to_check)
-            return True
-        except:  # (TypeError, ValueError):
-            pass
-
-        return False
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from FPGA_UART import *
+from Check_if import *
 
 
 if __name__ == "__main__":
     # Example usage
-    fpga = FPGA_UART(port="/dev/ttyUSB0", baud_rate=115200, timeout=1)
+    fpga = FPGA_UART(port="COM16", baud_rate=115200, timeout=1)
+
     fpga.open_instrument()
-    fpga.set_memory_addr(0x10)
-    fpga.write_val_mem(1234)
-    fpga.display_mem_vals_leds(1234)
+    fpga.set_memory_addr(0x00)
+    fpga.write_val_mem(0x55)
+    fpga.display_mem_vals_leds(0xAA)
+    
     mem_val = fpga.read_mem_val()
     print("Read memory value:", mem_val)
+    
     fpga.close_instrument()
